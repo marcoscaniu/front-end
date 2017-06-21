4215c9b6a2ef5fcd2f06562ad5a56c8cc03ac410
marcos_caniu <caniumarcos51@gmail.com>
2017-06-21 18:06:25 -0400

    modificado

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,6 +26,11 @@ angular
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl',
+        controllerAs: 'login'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the sApp
+ */
+angular.module('sApp')
+  .controller('LoginCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
